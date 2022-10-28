68ff5cad6dfd7ce034b800ddb493b295a0a44df0
Lucas Ferreira <lsousaah@gmail.com>
2022-10-28 11:59:45 -0300

    add: documentação swagger

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,17 +1,21 @@
-import { IsNotEmpty } from "class-validator";
+ import { IsNotEmpty } from "class-validator";
 import { Postagem } from "../../postagem/entities/postagem.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name: "tb_temas"})
 export class Tema {
 
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number;
 
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
+    @ApiProperty()
     descricao: string;
 
     @OneToMany(() => Postagem, (postagem) => postagem.tema)
+    @ApiProperty()
     postagem: Postagem[];
 }
